80d5f6eb5774930758cec35a5bb86b2c0dda1f43
Dalton DeMaria <dalton.demaria@gmail.com>
2024-09-30 09:04:05 -0500

    chess tinkering; on to next stage of cirque

--- chess/__main__.py
+++ chess/__main__.py
@@ -13,16 +13,13 @@ def print_decipher_remove_column_letters(grid_list, final):
             # get distance from 'A' and reduce by one file (column on a chess board)
             val_ord = ord(value) - 65
             new_letter = get_new_cipher_value(val_ord, 1)
-            print(f"old letter: {value}, new letter: {new_letter}")
 
 
 def print_decipher(final, shift):
-    print(">>>>>> printing decipher without removal:")
     new_value = []
     for value in final:
         cur_val = ord(value) - 65
         new_letter = get_new_cipher_value(cur_val, shift)
-        print(f"old letter: {value}, new letter: {new_letter}")
         new_value.append(new_letter)
 
     return "".join(new_value)
@@ -34,14 +31,17 @@ def get_new_cipher_value(ord_val, shift_count):
 
 
 # name of file filled in chess board from left to right, top to bottom
-# grid_string = "FHXJQVZTFIUSUVXRLHOSQQVITCOQTHLDNEUAUOJZVHDEOSOQYLZJHELEDJIEPZAP"
+#grid_string = "FHXJQVZTFIUSUVXRLHOSQQVITCOQTHLDNEUAUOJZVHDEOSOQYLZJHELEDJIEPZAP"
+
 # name of file filled in chess board grid via a1-h1, a2-h2... order
-grid_string = "DJIEPZAPYLZJHELEVHDEOSOQNEUAUOJZTCOQTHLDLHOSQQVIFIUSUVXRFHXJQVZT"
-# response with algebraic notation
+#grid_string = "DJIEPZAPYLZJHELEVHDEOSOQNEUAUOJZTCOQTHLDLHOSQQVIFIUSUVXRFHXJQVZT"
+grid_string = "TRIDZQEPZXVLJOLAVVQHOSEZQUQTUOHPJSSQAEJEXUOOUDZIHIHCEHLJFFLTNVYD"
+# response with algebraic notationa
 coordinate_string = "B8C6E5G6H4F5H6G4F6H5F4E6G5"
 # full response
 # coordinate_string = "B8C6C6E5E5G6G6H4H4F5F5H6H6G4G4F6F6H5H5F4F4E6E6G5G5F3F3D2D2B3B3A1A1C2C2B4"
-
+deciphered_gridstring = print_decipher(list(grid_string), 1)
+print(f"deciphered grid_string: {deciphered_gridstring}")
 
 grid_list = np.array(list(grid_string))
 grid = np.reshape(grid_list, (8, 8))
@@ -49,44 +49,19 @@ print(grid)
 
 coord_list = np.array(list(coordinate_string))
 X_coords = coord_list[::2]
-X_ascii = [ord(x)-65 for x in X_coords]
+X_ascii = [(ord(x) - 65) for x in X_coords]
 Y_coords = coord_list[1::2]
-Y_coords = [(8 - int(y)) for y in Y_coords]
+Y_coords = [(8-int(y)) for y in Y_coords]
 coords = zip(Y_coords, X_ascii)
 final = []
 for coord in coords:
-    print(f"coord: {coord}")
     final.append(str(grid[coord]))
 
 final_as_string = "".join(final)
 print(f"final: {final} -> as string: {final_as_string}")
 
-
-# print_decipher_remove_column_letters(grid_list, final)
-# for i in range(0, 26):
-#    print_decipher(final, i)
 result = print_decipher(final, 1)
 print(f"result: {result}")
 sorted = sorted(list(result))
 print(f"sorted result: {sorted}")
 
-
-def vigenere_cipher(text, keyword):
-    keyword_length = len(keyword)
-    return ''.join(
-        chr((ord(t) - 65 + ord(keyword[i % keyword_length]) - 65) % 26 + 65)
-        for i, t in enumerate(text)
-    )
-
-
-keyword = "WOOLWINDER"  # Keyword related to the artist
-ciphertext = "HDULDOELSHUJ"
-decrypted_text = vigenere_cipher(final_as_string, keyword)
-ai_decrypted_text = vigenere_cipher(ciphertext, keyword)
-try_three_text = vigenere_cipher(result, keyword)
-try_four = vigenere_cipher("XVQQZQQZQQZQ", keyword)
-print(f"Decrypted with keyword '{keyword}': {decrypted_text}")
-print(f"Decrypted with keyword (AI answer) '{keyword}' : {ai_decrypted_text}")
-print(f"Decrypted with keyword try four '{keyword}' : {try_four}")
-print(
-    f"Decrypted with keyword (shifted answer) '{keyword}' : {try_three_text}")
--- cirque_mystique/__main__.py
+++ cirque_mystique/__main__.py
@@ -1,22 +1,255 @@
-
+import requests
+import string
+import numpy
+# -----------------
+# Step 1
 # hippo holding an ear          -- caterpillar in front of apple -- llama holding an eggplant
-# beaver holding a rose         -- fox on an anvil               -- dapper crocodile with a ball
+# beaver holding a rose         -- fox on an anvil               -- dapper crocodile with a ball(orange?)
 # koala eating ramen            -- dolphin holding a lightbulb   -- giraffe playing guitar in a tophat
 # elephant with a record player -- iguana on a toaster           -- leopard holding an umbrella
+letters_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+all_letters = set(letters_string)
 
 def pretty_print(st, name):
     result = sorted(list(st))
     print(f"{name}: {result}")
 
+def print_letters_from_numbers(nums):
+    result = []
+    letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    for num in nums:
+        value = int(num)
+        result.append(letters[value-1])
 
-all_letters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return result
+
 animals = set("HCLBFAKDGEIJ")
 items = set("EAERABNLGMTU")
-
 
 combined = animals.union(items)
 
 minus_both = all_letters.difference(combined)
-pretty_print(animals, "animals")
-pretty_print(items, "items")
-pretty_print(minus_both, "missing letters")
+# pretty_print(animals, "animals")
+# pretty_print(items, "items")
+# pretty_print(minus_both, "missing letters")
+# BRALRAGETUNE
+# BRALRAGETUNA
+# ORALRAGETUNA
+# ORALRAGETUNE
+# ORALPAGETUNA
+# ORALPAGETUNE (!!)
+# -----------------
+
+# -----------------
+# Step 2
+# top-down, left then right
+# numbers_string = "08-19-05-07-01-03-15-23-16-09-20-25"
+# clockwise
+numbers_string = "03-01-07-05-19-08-15-23-16-09-20-25"
+# left-to-right, top to bottom
+# numbers_string = "08-15-19-23-05-16-07-09-01-20-03-25"
+# circular pattern - arcing from center left to center right, top then bottom (crystal ball pattern? idk)
+# numbers_string = "04-05-19-08-15-23-07-01-03-25-20-09"
+
+# CAGESHOWPITY (!!)
+
+
+number_list = numbers_string.split("-")
+print(f"numbers-list: {number_list}")
+
+step_2_result = print_letters_from_numbers(number_list)
+print(f"result: {step_2_result}")
+print("".join(step_2_result))
+
+# -----------------
+#  Step 3
+# first letter (roygbiv) -> second letter -> shape (s=square, c=circle, h=hex, t=triangle)
+# rs + rs = rs (rs = 0)
+# os + os = ys
+# os + ys = gs
+# ys + ys = bs
+# gs + gs = vs
+# ys + gs = is
+# vs + os = rc
+# yc + rc = yt
+# ic + rt = ih
+# gt + rt = gs
+# vh + os = rs
+# gt + it = oc
+# oh + bc = is
+# gh + ic + bt = ih
+
+# ih - ic = gs - rt
+# gs = ih + rt - ic
+# gt = ih - ic
+
+# gt = 3 - rt
+# rt = gs - gt
+# rt = ih - ic
+# ih - ic = 3 - gt
+# ih + gt = 3 + ic
+
+# derived
+# rt = gh + bt
+# rt = ih - ic
+# ih = gh + bt + ic
+# oh + bc = ys + gs
+# oh + bc = 5
+# 3(os) = gs
+# 4(os) = bs
+# 5(os) = is
+# 6(os) = vs
+# 7(os) = rc
+# 7(os) + yc = yt
+# oh = 5(os) - bc
+# bc = 5(os) - oh
+# gt + rt = 3(os)
+# gt = 3 - rt
+# gt = oc - it
+
+# rt = ih - ic
+# rt = gs - gt
+
+# ih - ic = gs - gt
+
+# yt - yc = 7
+# yt
+
+# unique potion types
+# red => square, circle, triangle
+# orange => square, circle, hex
+# yellow => square, circle, triangle
+# green => square, triangle, hex
+# blue => square, circle, triangle
+# indigo => square, circle, hex, triangle
+# violet => square, hex
+
+
+
+# bottom seems like a cipher key so mod 28 -> 7 colors, 4 shapes. seems to fit 
+# plugged the values in after all of the coincidences to verify -- success!
+
+def get_value(num):
+    cipher_values = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ?!")
+    return cipher_values[num % 28]
+
+def print_cipher_row(lst, index):
+    row = []
+    for square in lst:
+        next_val = sum(square)
+        row.append(get_value(next_val))
+    print(f"row[{index}] {row}")
+
+# squares =>   r=0,  o=1,   y=2,   g=3,   b=4,  i=5,  v=6
+# circles =>   r=7,  o=8,   y=9,   g=10,  b=11, i=12, v=13
+# triangles => r=14, o=15,  y=16,  g=17,  b=18, i=19, v=20
+# hex =>       r=21, o=22,  y=23,  g=24,  b=25, i=26, v=27
+
+value_dict = { 
+              "rs": 0,
+              "os": 1,
+              "ys": 2,
+              "gs": 3,
+              "bs": 4,
+              "is": 5,
+              "vs": 6,
+
+              "rc": 7,
+              "oc": 8,
+              "yc": 9,
+              "gc": 10,
+              "bc": 11,
+              "ic": 12,
+              "vc": 13,
+
+              "rt": 14,
+              "ot": 15,
+              "yt": 16,
+              "gt": 17,
+              "bt": 18,
+              "it": 19,
+              "vt": 20,
+
+              "rh": 21,
+              "oh": 22,
+              "yh": 23,
+              "gh": 24,
+              "bh": 25,
+              "ih": 26,
+              "vh": 27,
+              }
+
+
+def convert_to_values(lst):
+    return [value_dict[val] for val in lst]
+
+
+def rotated_values(lst):
+    flattened = flatten_list(lst)
+    print(f"flattened: {flattened}")
+    rotated = numpy.roll(flattened, -1)
+    print(f"rotated: {rotated}")
+    return regroup_values(rotated)
+
+
+def flatten_list(lst):
+    return [x for xs in lst for x in xs]
+
+
+def regroup_values(lst):
+    if len(lst) != 12:
+        return []
+    result = [[], [], [], []]
+    for val in range(0, 3):
+        result[0].append(lst[val])
+    for val in range(3, 6):
+        result[1].append(lst[val])
+    for val in range(6, 9):
+        result[2].append(lst[val])
+    for val in range(9, 12):
+        result[3].append(lst[val])
+
+    return result
+
+
+row_1_flasks = [["yh", "gc", "ic"], ["oc", "oc", "ih"], ["ot", "ih", "vs"], ["gh", "gt", "rt"]]
+row_2_flasks = [["yc", "yh", "rc"], ["bt", "bc", "gs"], ["oh", "rc", "bs"], ["it", "bh", "gs"]]
+row_3_flasks = [["vh", "is", "vh"], ["vc", "os", "gs"], ["rc", "yh", "ic"], ["ic", "rc", "gh"]]
+row_4_flasks = [["vh", "bh", "ot"], ["vt", "ic", "gh"], ["gh", "vh", "yh"], ["gh", "rs", "yh"]]
+
+row_1_values = [convert_to_values(val) for val in row_1_flasks]
+row_2_values = [convert_to_values(val) for val in row_2_flasks]
+row_3_values = [convert_to_values(val) for val in row_3_flasks]
+row_4_values = [convert_to_values(val) for val in row_4_flasks]
+
+print("------------ phase 1 -------------")
+print_cipher_row(row_1_values, 1)
+print_cipher_row(row_2_values, 2)
+print_cipher_row(row_3_values, 3)
+print_cipher_row(row_4_values, 4)
+
+row_1_values_rotated = rotated_values(row_1_flasks)
+row_2_values_rotated = rotated_values(row_2_flasks)
+row_3_values_rotated = rotated_values(row_3_flasks)
+row_4_values_rotated = rotated_values(row_4_flasks)
+print("----------------- rotated values ----------------")
+print(row_1_values_rotated)
+print(row_2_values_rotated)
+print(row_3_values_rotated)
+print(row_4_values_rotated)
+
+row_1_values_converted = [convert_to_values(val) for val in row_1_values_rotated]
+row_2_values_converted = [convert_to_values(val) for val in row_2_values_rotated]
+row_3_values_converted = [convert_to_values(val) for val in row_3_values_rotated]
+row_4_values_converted = [convert_to_values(val) for val in row_4_values_rotated]
+
+print("------------ phase 1 rotated left -------------")
+print_cipher_row(row_1_values_converted, 1)
+print_cipher_row(row_2_values_converted, 2)
+print_cipher_row(row_3_values_converted, 3)
+print_cipher_row(row_4_values_converted, 4)
+
+# CUREVILEACTS!!
+
+# ------------------------
+
